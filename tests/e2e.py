ca4da339f6495e227cea1c53fe49c6a7e959a985
Taras <38734137+TarasNon@users.noreply.github.com>
2023-01-29 17:05:36 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
-driver = webdriver.Chrome('chromedriver')
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 
 def test_scores_service():
-    driver.get("http://127.0.0.1:5000")
+    driver.get("http://3.125.145.138:8777")
     score_element = int(driver.find_element_by_id("score").text)
     if 1000 >= score_element >= 1:
         return True
