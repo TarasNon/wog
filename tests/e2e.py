7794ed1a1b6ee95d4c0c6df0dfcc7b2ad4a83f5c
Taras <38734137+TarasNon@users.noreply.github.com>
2023-01-29 17:19:27 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome('chromedriver')
 
 
 
