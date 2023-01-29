6dc8ceb713983285fb839dde10fa9f4694f27025
Taras <38734137+TarasNon@users.noreply.github.com>
2023-01-29 17:40:33 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -6,20 +6,13 @@ driver = webdriver.Chrome('tests\\chromedriver')
 
 
 def test_scores_service():
-    driver.get("http://3.125.145.138:8777")
-    score_element = int(driver.find_element_by_id("score").text)
+    driver.get("http://127.0.0.1:5000")
+    score_element = int(driver.find_element_by_id("score"))
     if 1000 >= score_element >= 1:
         return True
     else:
         return False
 
 
-def main_function():
-    test = test_scores_service()
-    if test:
-        return 0
-    else:
-        return -1
 
-
-main_function()
+test_scores_service()
