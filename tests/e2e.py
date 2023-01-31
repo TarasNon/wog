2d1eb7a8ba10b5b8119a619c75761eb387098700
Taras <38734137+TarasNon@users.noreply.github.com>
2023-01-31 23:51:22 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -3,24 +3,14 @@ from selenium.webdriver.chrome.service import Service
 
 s = Service('tests/chromedriver')
 driver = webdriver.Chrome(service=s)
-driver.get('http://127.0.0.1:5000')
 
 
 
-score_element = int(driver.find_element_by_id("score"))
-if 1000 >= score_element >= 1:
-    print("Test OK!")
-else:
-    print("Test BAD")
+def test_scores_service():
+    driver.get('http://127.0.0.1:5000')
+    score_element = int(driver.find_element_by_id("score"))
 
-
-
-# def main_function():
-#     test = test_scores_service()
-#     if test:
-#         return 0
-#     else:
-#         return -1
-
-
-# main_function()
+    if 1000 >= score_element >= 1:
+        print("Test OK!")
+    else:
+        print("Test BAD")
