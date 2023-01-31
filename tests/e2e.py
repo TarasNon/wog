ec4cc0e53698f97aba9929f663e1c3b4827d416a
Taras <38734137+TarasNon@users.noreply.github.com>
2023-01-31 16:02:06 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path=r'tests/chromedriver.exe')
+#driver = webdriver.Chrome(executable_path=r'tests/chromedriver.exe')
+driver = webdriver.Chrome('chromedriver')
 driver.get('http://3.125.145.138:8777')
 
 def test_scores_service():
