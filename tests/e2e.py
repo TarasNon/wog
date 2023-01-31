3701c8b9821f564d3ef4e5d661f8df316f46c9eb
Taras <38734137+TarasNon@users.noreply.github.com>
2023-01-31 23:17:04 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-s = Service('tests\\chromedriver')
+s = Service('chromedriver')
 driver = webdriver.Chrome(service=s)
 driver.get('http://3.125.145.138:8777')
 
